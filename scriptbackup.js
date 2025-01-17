c3aab7cb1e3bdd1fddca3585b6ca8c5e944e3e5a
Arti <59352535+artifishvr@users.noreply.github.com>
2025-01-17 10:24:57 -0800

    first commit

---
+++ scriptbackup.js
@@ -0,0 +1,790 @@
+const notes = [
+  {
+    duration: 0.31034475,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 0,
+    time: 0,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034474999999995,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 192,
+    time: 0.2068965,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 288,
+    time: 0.31034475,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 384,
+    time: 0.413793,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 480,
+    time: 0.51724125,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 576,
+    time: 0.6206895,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 576,
+    time: 0.6206895,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034474999999995,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 672,
+    time: 0.72413775,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034474999999995,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 768,
+    time: 0.827586,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 960,
+    time: 1.0344825,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 1056,
+    time: 1.13793075,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 1152,
+    time: 1.241379,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 1248,
+    time: 1.34482725,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 1344,
+    time: 1.4482755,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 1344,
+    time: 1.4482755,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 1440,
+    time: 1.55172375,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 1536,
+    time: 1.655172,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034474999999984,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 1728,
+    time: 1.8620685000000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034474999999984,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 1824,
+    time: 1.9655167500000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 1920,
+    time: 2.068965,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 2016,
+    time: 2.17241325,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 2112,
+    time: 2.2758615,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 35,
+    name: "B1",
+    ticks: 2112,
+    time: 2.2758615,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 2208,
+    time: 2.37930975,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 37,
+    name: "C#2",
+    ticks: 2304,
+    time: 2.482758,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 37,
+    name: "C#2",
+    ticks: 2496,
+    time: 2.6896545,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 2592,
+    time: 2.79310275,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 37,
+    name: "C#2",
+    ticks: 2688,
+    time: 2.896551,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 2784,
+    time: 2.99999925,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 2880,
+    time: 3.1034475,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 37,
+    name: "C#2",
+    ticks: 2880,
+    time: 3.1034475,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 2976,
+    time: 3.20689575,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 3072,
+    time: 3.310344,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 3264,
+    time: 3.5172405,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 3360,
+    time: 3.6206887500000002,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 3456,
+    time: 3.7241370000000003,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 3552,
+    time: 3.8275852500000003,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 3648,
+    time: 3.9310335000000003,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.31034475000000006,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 3648,
+    time: 3.9310335000000003,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 3744,
+    time: 4.03448175,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 3840,
+    time: 4.13793,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4032,
+    time: 4.3448265,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 4128,
+    time: 4.44827475,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4224,
+    time: 4.551723,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 4320,
+    time: 4.65517125,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 4416,
+    time: 4.7586195,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4416,
+    time: 4.7586195,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 4512,
+    time: 4.8620677500000005,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4608,
+    time: 4.965516,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4800,
+    time: 5.1724125,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 4896,
+    time: 5.2758607500000005,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 4992,
+    time: 5.379309,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 5088,
+    time: 5.482757250000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 5184,
+    time: 5.5862055,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 5184,
+    time: 5.5862055,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 5280,
+    time: 5.689653750000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 5376,
+    time: 5.793102,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 5568,
+    time: 5.9999985,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 5664,
+    time: 6.103446750000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447500000005,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 5760,
+    time: 6.206895,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 5856,
+    time: 6.310343250000001,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 49,
+    name: "C#3",
+    ticks: 5952,
+    time: 6.4137915,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 39,
+    name: "D#2",
+    ticks: 5952,
+    time: 6.4137915,
+    velocity: 0.3937007874015748,
+  },
+  {
+    duration: 0.3103447499999996,
+    durationTicks: 288,
+    midi: 51,
+    name: "D#3",
+    ticks: 6048,
+    time: 6.517239750000001,
+    velocity: 0.3937007874015748,
+  },
+];
+
+function createNotePlayer() {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Convert MIDI note to frequency
+  function midiToFreq(midi) {
+    return 440 * Math.pow(2, (midi - 69) / 12);
+  }
+
+  function playNote(midiNote, time, duration, velocity) {
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.type = "triangle";
+    oscillator.frequency.value = midiToFreq(midiNote);
+
+    const startTime = audioContext.currentTime + time;
+    const attackTime = 0.005;
+    const releaseTime = 0.015;
+
+    // Initialize gain at 0
+    gainNode.gain.setValueAtTime(0, startTime);
+    // Fade in
+    gainNode.gain.linearRampToValueAtTime(
+      velocity * 0.3,
+      startTime + attackTime
+    );
+    // Fade out
+    gainNode.gain.linearRampToValueAtTime(0, startTime + duration);
+
+    oscillator.start(startTime);
+    oscillator.stop(startTime + duration + releaseTime);
+  }
+  // Play the sequence
+  function playSequence(notes, offset = 0) {
+    notes.forEach((note) => {
+      playNote(note.midi, note.time + offset, note.duration, note.velocity);
+    });
+  }
+
+  return {
+    playSequence,
+    audioContext,
+  };
+}
+
+window.onload = function () {
+  const button = document.createElement("button");
+  button.textContent = "obligatory bad apple";
+
+  button.onclick = function () {
+    const player = createNotePlayer();
+    player.playSequence(notes);
+    setInterval(() => {
+      player.playSequence(notes);
+    }, 6550);
+  };
+
+  document.body.appendChild(button);
+  // Create canvas element
+  const canvas = document.createElement("canvas");
+  canvas.width = 400;
+  canvas.height = 400;
+  canvas.style.display = "block";
+  canvas.style.margin = "auto";
+
+  // Add canvas to document
+  document.body.appendChild(canvas);
+
+  // Get context for drawing
+  const context = canvas.getContext("2d");
+
+  // the canvas width & height, snake x & y, and the apple x & y, all need to be a multiples of the grid size in order for collision detection to work
+  // (e.g. 16 * 25 = 400)
+  var grid = 16;
+  var count = 0;
+
+  var snake = {
+    x: 160,
+    y: 160,
+
+    // snake velocity. moves one grid length every frame in either the x or y direction
+    dx: grid,
+    dy: 0,
+
+    // keep track of all grids the snake body occupies
+    cells: [],
+
+    // length of the snake. grows when eating an apple
+    maxCells: 4,
+  };
+  var apple = {
+    x: 320,
+    y: 320,
+  };
+
+  // get random whole numbers in a specific range
+  // @see https://stackoverflow.com/a/1527820/2124254
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  // game loop
+  function loop() {
+    requestAnimationFrame(loop);
+
+    // slow game loop to 15 fps instead of 60 (60/15 = 4)
+    if (++count < 4) {
+      return;
+    }
+
+    count = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // move snake by it's velocity
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+
+    // wrap snake position horizontally on edge of screen
+    if (snake.x < 0) {
+      snake.x = canvas.width - grid;
+    } else if (snake.x >= canvas.width) {
+      snake.x = 0;
+    }
+
+    // wrap snake position vertically on edge of screen
+    if (snake.y < 0) {
+      snake.y = canvas.height - grid;
+    } else if (snake.y >= canvas.height) {
+      snake.y = 0;
+    }
+
+    // keep track of where snake has been. front of the array is always the head
+    snake.cells.unshift({ x: snake.x, y: snake.y });
+
+    // remove cells as we move away from them
+    if (snake.cells.length > snake.maxCells) {
+      snake.cells.pop();
+    }
+
+    // draw apple
+    context.fillStyle = "red";
+    context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+
+    // draw snake one cell at a time
+    context.fillStyle = "green";
+    snake.cells.forEach(function (cell, index) {
+      // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
+      context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+      // snake ate apple
+      if (cell.x === apple.x && cell.y === apple.y) {
+        snake.maxCells++;
+
+        // canvas is 400x400 which is 25x25 grids
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+      }
+
+      // check collision with all cells after this one (modified bubble sort)
+      for (var i = index + 1; i < snake.cells.length; i++) {
+        // snake occupies same space as a body part. reset game
+        if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+          snake.x = 160;
+          snake.y = 160;
+          snake.cells = [];
+          snake.maxCells = 4;
+          snake.dx = grid;
+          snake.dy = 0;
+
+          apple.x = getRandomInt(0, 25) * grid;
+          apple.y = getRandomInt(0, 25) * grid;
+        }
+      }
+    });
+  }
+
+  // listen to keyboard events to move the snake
+  document.addEventListener("keydown", function (e) {
+    // prevent snake from backtracking on itself by checking that it's
+    // not already moving on the same axis (pressing left while moving
+    // left won't do anything, and pressing right while moving left
+    // shouldn't let you collide with your own body)
+
+    // left arrow key
+    if (e.which === 37 && snake.dx === 0) {
+      snake.dx = -grid;
+      snake.dy = 0;
+    }
+    // up arrow key
+    else if (e.which === 38 && snake.dy === 0) {
+      snake.dy = -grid;
+      snake.dx = 0;
+    }
+    // right arrow key
+    else if (e.which === 39 && snake.dx === 0) {
+      snake.dx = grid;
+      snake.dy = 0;
+    }
+    // down arrow key
+    else if (e.which === 40 && snake.dy === 0) {
+      snake.dy = grid;
+      snake.dx = 0;
+    }
+  });
+
+  // start the game
+  requestAnimationFrame(loop);
+};
